7234ded36bde879ee2eb06b8f461be8a8872d22e
andrestobar05 <andrewtobar05@gmail.com>
2025-05-29 18:12:18 -0600

    Define navigation routes and state management
    This commit introduces constants for screen navigation and parameters in `AppRoutes.kt`.
    It also creates `AppState.kt` to manage navigation actions like `popUp`, `navigate`, `navigateAndPopUp`, and `clearAndNavigate` using `NavHostController`.
    
    Key additions:
    - `AppRoutes.kt`: Defines screen names (e.g., `SCAN_SCREEN`, `RESULT_SCREEN`), argument keys (`ITEM_ID`), and network constants (`LOCALHOST`, `AUTH_PORT`, `FIRESTORE_PORT`).
    - `AppState.kt`: Provides a stable class to encapsulate navigation logic, simplifying navigation calls within the application.

---
+++ app/src/main/java/com/example/dondeva/AppRoutes.kt
@@ -0,0 +1,18 @@
+package com.example.dondeva
+
+// Pantallas
+const val SCAN_SCREEN = "ScanScreen"
+const val RESULT_SCREEN = "ResultScreen"
+const val HISTORY_SCREEN = "HistoryScreen"
+const val SIGN_IN_SCREEN = "SignInScreen"
+const val SIGN_UP_SCREEN = "SignUpScreen"
+const val SPLASH_SCREEN = "SplashScreen"
+
+// Argumentos
+const val ITEM_ID = "itemId"
+const val ITEM_ID_ARG = "?$ITEM_ID={$ITEM_ID}"
+const val ITEM_DEFAULT_ID = "-1"
+
+const val LOCALHOST = "10.0.2.2"
+const val AUTH_PORT = 9099
+const val FIRESTORE_PORT = 8080
