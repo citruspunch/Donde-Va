d8c06bc71f18f1b5f1909bd7294b1be02f7c5eb5
andrestobar05 <andrewtobar05@gmail.com>
2025-05-30 17:13:51 -0600

    Refactor StorageService to fetch garbage items for the current user
    This commit modifies the `StorageService` to directly provide a Flow of `GarbageItem`s associated with the currently authenticated user.
    
    Key changes:
    - Introduced `garbageItemsByCurrentUser` Flow in `StorageService` interface and its implementation `StorageServiceImpl`. This Flow automatically updates when the current user changes or their garbage items are modified in Firestore.
    - The `garbageItemsByCurrentUser` Flow leverages `auth.currentUser.flatMapLatest` to react to user changes and then queries the "garbageItems" collection in Firestore, filtering by the current user's ID.
    - Removed the `garbageItemsByUser(userId: String)` function as its functionality is now encapsulated within `garbageItemsByCurrentUser`.

--- app/src/main/java/com/example/dondeva/data/impl/StorageServiceImpl.kt
+++ app/src/main/java/com/example/dondeva/data/impl/StorageServiceImpl.kt
@@ -7,10 +7,21 @@ import kotlinx.coroutines.flow.Flow
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.dataObjects
 import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.tasks.await
 
 
 class StorageServiceImpl constructor(private val auth: AccountService) : StorageService {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val garbageItemsByCurrentUser: Flow<List<GarbageItem>>
+        get() = auth.currentUser.flatMapLatest { user ->
+            Firebase.firestore
+                .collection(GARBAGE_ITEMS_COLLECTION)
+                .whereEqualTo(USER_ID_FIELD, user?.id)
+                .dataObjects()
+        }
+
     override suspend fun createGarbageItem(garbageItem: GarbageItem) {
         val garbageItemWithUserId = garbageItem.copy(userId = auth.currentUserId)
         Firebase.firestore
@@ -28,13 +39,6 @@ class StorageServiceImpl constructor(private val auth: AccountService) : Storage
         return Firebase.firestore
             .collection(GARBAGE_ITEMS_COLLECTION)
             .document(itemId).get().await().toObject(GarbageItem::class.java)
-    }
-
-    override fun garbageItemsByUser(userId: String): Flow<List<GarbageItem>> {
-        return Firebase.firestore
-            .collection(GARBAGE_ITEMS_COLLECTION)
-            .whereEqualTo(USER_ID_FIELD, userId)
-            .dataObjects()
     }
 
     companion object {
